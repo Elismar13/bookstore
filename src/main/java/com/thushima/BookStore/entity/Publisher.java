b461a457fc2a6fe1915358d3af9e42dafd4f528a
Elismar <elismarsilva.5@outlook.com>
2021-04-13 21:21:09 -0300

    feat: 🎸 publisher mapper entity

---
+++ src/main/java/com/thushima/BookStore/entity/Publisher.java
@@ -0,0 +1,28 @@
+package com.thushima.BookStore.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private Long year_of_creation;
+
+}
