7a63343f7a7d66df56fc0bb6d1ab6f8b7bec9d44
Elismar <elismarsilva.5@outlook.com>
2021-04-13 21:21:28 -0300

    feat: 🎸 book entity relational mapper

---
+++ src/main/java/com/thushima/BookStore/entity/Book.java
@@ -0,0 +1,34 @@
+package com.thushima.BookStore.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private Long pages;
+
+    @Column(nullable = false)
+    private String publisher;
+
+}
